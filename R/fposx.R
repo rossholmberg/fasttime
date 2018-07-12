a2f4f7c86ed0d326f7fdd5bd9102fc7578412f4c
rossholmberg <ross@holmberg.com.au>
2018-07-12 23:44:20 +1000

    Adding conversion method for `POSIXct` object to `fposx`

--- R/fposx.R
+++ R/fposx.R
@@ -11,8 +11,20 @@ fposx <- function(x, required.components = 3L) {
                                     as.character(x),
                                     required.components),
                          tz = "UTC" )
+    } else if( inherits( x, "POSIXct" ) ) {
+        if( attr( x, "tzone" ) == "UTC" ) {
+            out <- x
+        } else if( attr( x, "tzone" ) == "GMT" ) {
+            out <- x
+            attr( out, "tzone" ) <- "UTC"
+            warning( "`tzone` attribute changed from GMT to UTC. This should not cause any shift." )
+        } else {
+            stop( "To convert to fposx, the timezone MUST be UTC." )
+        }
     } else if( is.numeric( x ) ) {
-        out <- .POSIXct( xx = x, tz = "UTC" )
+        out <- as.POSIXct.numeric( xx = x,
+                                   tz = "UTC",
+                                   origin = "1970-01-01 00:00:00" )
     } else {
         stop( "Don't know how to convert to fposx" )
     }
